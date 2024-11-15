474b9f9d7e1464190a50acbe0ff916aa387c08cd
Yamauchi-Kazuma <kazuma0424@stu.kanazawa-u.ac.jp>
2024-11-15 23:20:37 +0900

    変更

---
+++ m1-07.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>mission_1-7</title>
+</head>
+<body>
+<?php $number = 4;
+      echo $number ."<br>";
+      $number = 5;
+      echo $number;
+      echo "<br>"?>
+      改行成功
+</body>
+</html>
